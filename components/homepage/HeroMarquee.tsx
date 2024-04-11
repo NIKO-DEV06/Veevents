c39b6a28531c35485adb772f989cb8b3d9033992
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2024-04-11 16:09:45 +0100

    added hero text blur with rotate

--- components/homepage/HeroMarquee.tsx
+++ components/homepage/HeroMarquee.tsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee";
 const HeroMarquee = () => {
   return (
     <>
-      <div className="absolute top-[8rem] z-[-10] opacity-[0.1] w-[100vw] 2xl:max-w-[1470px]">
+      <div className="absolute blur-[2px] rotate-45 top-[8rem] z-[-10] opacity-[0.1] w-[100vw] 2xl:max-w-[1470px]">
         <Marquee gradient={false} direction="right" speed={25}>
           {Array.from({ length: 15 }, (_, index) => (
             <p
@@ -15,7 +15,7 @@ const HeroMarquee = () => {
           ))}
         </Marquee>
       </div>
-      <div className="absolute top-[31rem] z-[-10] opacity-[0.1] w-[100vw] 2xl:max-w-[1470px]">
+      <div className="absolute blur-[2px] rotate-[-25deg] top-[31rem] z-[-10] opacity-[0.1] w-[100vw] 2xl:max-w-[1470px]">
         <Marquee gradient={false} direction="left" speed={25}>
           {Array.from({ length: 15 }, (_, index) => (
             <p
